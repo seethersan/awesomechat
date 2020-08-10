fdddc69450d3524a4ad1806d7f3a5327785051df
seethersan <carlos_jcez@hotmail.com>
2020-08-10 06:10:20 -0500

    [feature] added signUp and signIn methods to UserAuth

--- awesomechat-frontend/src/components/UserAuth.vue
+++ awesomechat-frontend/src/components/UserAuth.vue
@@ -66,6 +66,29 @@ export default {
     return {
       email: '', username: '', password: ''
     }
+  },
+
+  methods: {
+    signUp () {
+      $.post('http://localhost:8000/auth/users/create/', this.$data, (data) => {
+        alert('Your account has been created. You will be signed in automatically')
+        this.signIn()
+      }).fail((response) => {
+        alert(response.responseText)
+      })
+    },
+
+    signIn () {
+      const credentials = {username: this.username, password: this.password}
+
+      $.post('http://localhost:8000/auth/token/create/', credentials, (data) => {
+        sessionStorage.setItem('authToken', data.auth_token)
+        sessionStorage.setItem('username', this.username)
+        this.$router.push('/chats')
+      }).fail((response) => {
+        alert(response.responseText)
+      })
+    }
   }
 }
 </script>
